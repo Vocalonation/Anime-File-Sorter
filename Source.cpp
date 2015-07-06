f68ee60875c5daa98be59afb833bd9645a17c1b7
Aaron <ahctang@gmail.com>
2015-07-06 21:23:57 +0900

    Bug fix for iterator exploding due to faulty logic. However, program still crashes when processing the full directory.

--- Source.cpp
+++ Source.cpp
@@ -8,13 +8,13 @@
 using namespace boost::filesystem;
 
 struct SortFiles {
-	boost::filesystem::path FilePath;
-	std::string SortFolderName;
+	boost::filesystem::path OriginalFilePath;
+	boost::filesystem::path SortedFilePath;
 };
 
 int filepicker(boost::filesystem::path SortTargetDirectory, std::vector<SortFiles> & mkvvector, boost::filesystem::path SortDestinationDirectory);
-void directorymaker(std::vector <SortFiles> & mkvvector, boost::filesystem::path SortDestinationDirectory, std::vector<SortFiles> & mkvvectordirectory);
-bool filemover (std::vector<SortFiles> mkvvector, std::vector<SortFiles> mkvvectordirectory,boost::filesystem::path SortDestinationDirectory);
+bool filesorter(std::vector <SortFiles> & mkvvector, boost::filesystem::path SortDestinationDirectory);
+
 
 int main (int argc, char * argv[]) {
 
@@ -56,7 +56,7 @@ int main (int argc, char * argv[]) {
 
 	if (is_directory(SortTargetDirectory) == false) {
 		std::cout << "Target directory(" << SortTargetDirectory <<") is an invalid directory." << std::endl;
-		return 0;
+		return 1;
 	}
 	else {
 		std::cout << SortTargetDirectory << "is a valid directory." << std::endl;
@@ -64,7 +64,7 @@ int main (int argc, char * argv[]) {
 
 	if (is_directory(SortDestinationDirectory) == false) {
 		std::cout << "Destination directory(" << SortDestinationDirectory <<") is an invalid directory." << std::endl;
-		return 0;
+		return 1;
 	}
 	else {
 		std::cout << SortDestinationDirectory << "is a valid directory." << std::endl;
@@ -76,18 +76,13 @@ int main (int argc, char * argv[]) {
 	FileCounter = filepicker(SortTargetDirectory, mkvvector, SortDestinationDirectory);	
 	if (FileCounter <= 0) {
 		std::cout << "No files to sort in target directory." << std::endl;
-		exit(0);
+		return 1;
 	}
 	std::cout << "Sorting " << FileCounter << " files..." << std::endl;
 
 
-	//Make the folders based on the file name
-	directorymaker(mkvvector,SortDestinationDirectory,mkvvectordirectory);
-
-	std::cout << "Debug flag" << std::endl;
-
-	//Place the files in the corresponding directory. End the program in the case of an exception.
-	if (filemover(mkvvector, mkvvectordirectory,SortDestinationDirectory) == false) return 1;
+	//Make the folders based on the file name and move the files
+	if (filesorter(mkvvector,SortDestinationDirectory) == false) return 1; 	
 
 	//All done
 	std::cout << "Sorting Complete!" << std::endl;
@@ -111,16 +106,15 @@ int main (int argc, char * argv[]) {
 // Each file entry will be saved in a struct SortFiles containing the FilePath and the FileName. Function will iterate through the target directory and grab the pathes of all of the files which are .mkv and save them in a vector
 int filepicker(boost::filesystem::path SortTargetDirectory, std::vector<SortFiles> & mkvvector, boost::filesystem::path SortDestinationDirectory) {
 	int FileCounter = 0;
-	SortFiles mkvfile;	
+	SortFiles FileEntry;	
 
 	//Save 
 	for (directory_iterator itr(SortTargetDirectory); itr!= directory_iterator(); ++itr) {
 		if(itr->path().extension() == ".mkv") {
-			mkvfile.FilePath = itr->path();
-			mkvvector.push_back(mkvfile);
+			FileEntry.OriginalFilePath = itr->path();
+			mkvvector.push_back(FileEntry);
 			FileCounter++;
-		} 
-		
+		} 		
 	}
 	
 	std::ofstream outputfile;
@@ -140,7 +134,7 @@ int filepicker(boost::filesystem::path SortTargetDirectory, std::vector<SortFile
 	outputfile <<"Sorting " << FileCounter << " Files..." << std::endl;
 
 	for (std::vector<SortFiles>::const_iterator itr = mkvvector.begin(); itr != mkvvector.end(); ++itr) {
-		outputfile << itr->FilePath << std::endl;
+		outputfile << itr->OriginalFilePath << std::endl;
 	}
 	outputfile.close();
 
@@ -148,11 +142,22 @@ int filepicker(boost::filesystem::path SortTargetDirectory, std::vector<SortFile
 }
 	
 
-void directorymaker(std::vector <SortFiles> & mkvvector, boost::filesystem::path SortDestinationDirectory, std::vector<SortFiles> & mkvvectordirectory) {
+bool filesorter(std::vector <SortFiles> & mkvvector, boost::filesystem::path SortDestinationDirectory) {
 	
-	std::string FileName, FilePath, FolderName, FolderPath;	
-	path Foldernameworkaround;
-	SortFiles DestinationFiles;
+	std::string FileName, FilePath, FolderName, FolderPath;		
+	SortFiles FileEntry;
+
+	//Initialize ofstream for writing a log of sorted files
+	
+	std::ofstream outputfile;
+	std::string DestinationPath;
+	DestinationPath = SortDestinationDirectory.string() + "\\Sorted Files.txt";
+	outputfile.open(DestinationPath,std::ios::out | std::ios::trunc);
+
+	if(outputfile.bad() == true) {
+		std::cout << "Log file write failed." << std::endl;
+		return false;
+	}
 
 
 	for (std::vector<SortFiles>::iterator itr = mkvvector.begin(); itr != mkvvector.end(); ++itr) {
@@ -161,8 +166,7 @@ void directorymaker(std::vector <SortFiles> & mkvvector, boost::filesystem::path
 
 		//Create the folder name by pruning out underscores/fansub group name/chksum/resolution no./episode/etc. from the file name and save it in both mkvvector and mkvvectordirectory
 
-		Foldernameworkaround = (*itr).FilePath.stem();		
-		FileName = Foldernameworkaround.string();
+		FileName = itr->OriginalFilePath.stem().string();				
 		found2 = FileName.find_first_of(']');
 		FileName = FileName.substr(found2+2);		
 
@@ -174,35 +178,18 @@ void directorymaker(std::vector <SortFiles> & mkvvector, boost::filesystem::path
 
 		if (FileName.find_last_of('-') != std::string::npos){
 			found = FileName.find_last_of('-');
-			FolderName = FileName.substr(0,(found-1));
+			FolderName = FileName.substr(0,(found-1));			
 			
-			//Saving to mkvvector 
-			itr->SortFolderName = FolderName;
-			
-			//Saving to temporary struct which will be added to mkvvectordirectory
-			DestinationFiles.SortFolderName = FolderName;			
 		}
 
 		else if (FileName.find_first_of('[') != std::string::npos) {
 		 	found = FileName.find_first_of('[');
-		 	FolderName = FileName.substr(0,(found-1));		
-
-			//Saving to mkvvector 
-			itr->SortFolderName = FolderName;
-
-			//Saving to temporary struct which will be added to mkvvectordirectory
-			DestinationFiles.SortFolderName = FolderName;					
+		 	FolderName = FileName.substr(0,(found-1));								
 		}		
 		
 		else if (FileName.find_first_of('(') != std::string::npos) {
 			found = FileName.find_first_of('(');
-			FolderName = FileName.substr(0, (found - 1));
-
-			//Saving to mkvvector 
-			itr->SortFolderName = FolderName;
-
-			//Saving to temporary struct which will be added to mkvvectordirectory
-			DestinationFiles.SortFolderName = FolderName;			
+			FolderName = FileName.substr(0, (found - 1));			
 		}
 		
 		//Skip the file, if the file name somehow creates problems.
@@ -210,102 +197,72 @@ void directorymaker(std::vector <SortFiles> & mkvvector, boost::filesystem::path
 			continue;
 		}
 
-
 		
 		//Build folder path
 		FolderPath = SortDestinationDirectory.string() + "\\" + FolderName;
 		path p(FolderPath);		
 		
-		//Build File path
-		FilePath = FolderPath + "\\" + itr->FilePath.filename().string();
-		
-		//Save the paths of all of the created folders and/or existing folders in mkvvectordirectory
-		DestinationFiles.FilePath = FilePath;
-		mkvvectordirectory.push_back(DestinationFiles);
-		
-		//Create the folders if they don't exist
+		//Create the folders if they don't exist and add a bit to the log file if the folder is actually created
 		try {
-			create_directory(p);
+			if (create_directory(p) == true) {
+				outputfile << "Directory: " << FolderName << std::endl;
+			}
+
 		}
 
 		catch (filesystem_error& e) {
 			std::cerr << "Error: " << e.what() << std::endl;
-			exit(0);
+			return false;
 		}
+
+		//Build Sorted File path
+		FilePath = FolderPath + "\\" + itr->OriginalFilePath.filename().string();
+		path g(FilePath);
+
+		itr->SortedFilePath = g;
+
+		//Move the files using the newly created SortedFilePath and write a log of the sorted files
+		std::cout << "Now sorting: " << itr -> OriginalFilePath << std::endl;
+		
+		try {
+			rename(itr->OriginalFilePath, itr->SortedFilePath);
+		}
+
+		catch (filesystem_error& e) {
+			std::cerr << "Error: " << e.what() << std::endl;
+
+			//If rename fails copy file and catch any exceptions 
+			try {
+				copy_file(itr->OriginalFilePath, itr->SortedFilePath);
+			}
+
+			catch (filesystem_error& e) {
+				std::cerr << "Error: " << e.what() << std::endl;
+				outputfile.close();
+				return false;
+			}
+
+			//Delete existing file at old path and catch any exceptions
+			try {
+				remove(itr->OriginalFilePath);
+			}
+
+			catch (filesystem_error& e) {
+				std::cerr << "Error: " << e.what() << std::endl;
+				outputfile.close();
+				return false;
+			}
+		}
+
+		//Record all of the sorted files and their directory in a log.		 
+		outputfile << itr->SortedFilePath.filename() << std::endl;
+		outputfile << std::endl;
 	}	
 
-	return;
+	outputfile.close();
+
+	return true;
 }
 
-bool filemover(std::vector<SortFiles> mkvvector, std::vector<SortFiles> mkvvectordirectory, boost::filesystem::path SortDestinationDirectory) {
 
-	std::ofstream outputfile;
-	std::string DestinationPath;
-
-	DestinationPath = SortDestinationDirectory.string() + "\\Sorted Files.txt";
-	outputfile.open(DestinationPath,std::ios::out | std::ios::trunc);
-
-	if(outputfile.bad() == true) {
-		std::cout << "Log file write failed." << std::endl;
-		return 0;
-	}
-
-	//Match path in mkvvector with the correct folder path contained in mkvvectordirectory
- 	for (std::vector<SortFiles>::const_iterator itr = mkvvector.begin(); itr != mkvvector.end(); ++itr) {
- 		
- 		outputfile << "Directory: " << itr->SortFolderName << std::endl;
-
- 		for (std::vector<SortFiles>::const_iterator itr2 = mkvvectordirectory.begin(); itr2 != mkvvectordirectory.end(); ++itr2) {
-
- 			if (itr->SortFolderName == itr2->SortFolderName) {
-				std::cout << "Now sorting: " << itr->FilePath <<std::endl;
-
-				//Move the file to the new path and catch any exceptions
-				try {
-					rename(itr->FilePath, itr2->FilePath);
-				}
-
-				catch (filesystem_error& e) {
-					std::cerr << "Error: " << e.what() << std::endl;
-					
-					//If rename fails copy file and catch any exceptions 
-					try {
-						copy_file(itr->FilePath, itr2->FilePath);
-					}
-
-					catch (filesystem_error& e) {
-						std::cerr << "Error: " << e.what() << std::endl;
-						outputfile.close();
-						return false;
-					}
-
-					//Record all of the sorted files and their directory in a log.
-					outputfile << itr->FilePath.filename() << std::endl;
-					outputfile << std::endl;
-
-					//Delete existing file at old path and catch any exceptions
-					try {
-						remove(itr->FilePath);
-					}
-
-					catch (filesystem_error& e) {
-						std::cerr << "Error: " << e.what() << std::endl;
-						outputfile.close();
-						return false;					
-					}
-
-					//Record all of the sorted files and their directory in a log.
-					outputfile << itr->FilePath.filename() << std::endl;
-					outputfile << std::endl;
-				}				
-			
-				if (itr != mkvvector.end()) ++itr;
-			}			
- 		}
- 	}
-
- 	outputfile.close();
-	return true;
-
- }
 
